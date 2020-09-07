3dedc36471ed4a91adcbc419bd5aa476b610cf72
karanwadhwa <karanwadhwa0309@gmail.com>
2020-09-07 22:14:06 +0530

    Updates

---
+++ precache-manifest.fd20091af9714298bff2871d2aec844f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea9402586f10929cbfe57f4bb3f92904",
+    "url": "/BurgerBuilderUI/index.html"
+  },
+  {
+    "revision": "22ca14934da104eaf99e",
+    "url": "/BurgerBuilderUI/static/css/main.2cb24839.chunk.css"
+  },
+  {
+    "revision": "3bc582ad7f414c931724",
+    "url": "/BurgerBuilderUI/static/js/2.d7665eb6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BurgerBuilderUI/static/js/2.d7665eb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22ca14934da104eaf99e",
+    "url": "/BurgerBuilderUI/static/js/main.5fc1dbe0.chunk.js"
+  },
+  {
+    "revision": "0cb2e181cb20cb960050",
+    "url": "/BurgerBuilderUI/static/js/runtime-main.fe71fee7.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/BurgerBuilderUI/static/media/burger.b8503d26.png"
+  }
+]);
